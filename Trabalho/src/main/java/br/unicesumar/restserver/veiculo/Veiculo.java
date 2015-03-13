5ad134efc8aa0c2aead4d5bfef343e8432691a70
unknown <leonardorinaudo@gmail.com>
2015-03-12 21:05:47 -0300

    Arrumando nome da classe

--- Trabalho/src/main/java/br/unicesumar/restserver/veiculo/Veiculo.java
+++ Trabalho/src/main/java/br/unicesumar/restserver/veiculo/Veiculo.java
@@ -1,8 +1,14 @@
 package br.unicesumar.restserver.veiculo;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 
+@Entity 
 public class Veiculo {
+    @Id
+    private long id;
+    private string placa;
     
     
 }
